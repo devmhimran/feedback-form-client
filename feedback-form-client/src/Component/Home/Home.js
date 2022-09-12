f3aeb3fb5f2f128f59c7db593fc934742bcdf510
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-09-12 08:46:24 +0600

    Added Form Handler & Update HomeLeft

--- feedback-form-client/src/Component/Home/Home.js
+++ feedback-form-client/src/Component/Home/Home.js
@@ -4,46 +4,67 @@ import HomeLeft from './HomeLeft';
 import formImage from '../Assets/feedback__form__image__02.png';
 
 const Home = () => {
+    const handleForm = (e)=>{
+        e.preventDefault();
+        const email = e.target.email.value;
+        const firstName = e.target.firstName.value;
+        const lastName = e.target.lastName.value;
+        const address = e.target.address.value;
+        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        const d = new Date();
+        const formData = {
+            email,
+            firstName,
+            lastName,
+            address,
+            date: d.toLocaleDateString("en-US", options)
+        }
+        fetch('http://localhost:5000/feedback-form', {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify(formData)
+            }).then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                })
+        // fetch('http://localhost:5000/feedback-form', {
+        //     method: "POST",
+        //     headers: {
+        //         "content-type": "application/json"
+        //     },
+        //     body: JSON.stringify(formData)
+        // }).then(res => res.json())
+        // .then(body => console.log(body))
+        console.log(formData)
+    }
     return (
         <div className='feedback__main'>
             <div className="container">
                 <div className="feedback__form">
-                    <div className="feedback__form__left">
-                        <div className="feedback__form__left__main">
-                            <h1 className='heading'>
-                                Get Everything you need<br></br>
-                                for creating wordpress<br></br>
-                                website
-                            </h1>
-                            <img className='form__image' src={formImage} alt="" />
-                            <p className='feedback__form__bottom__text'>
-                                All-in-one Multifunctional subscribe <br></br>
-                                service perfect for launching all kinds of <br></br>
-                                wordpress projects
-                            </p>
-                        </div>
-                    </div>
+                   <HomeLeft></HomeLeft>
                     <div className="feedback__form__right">
                         <div className="feedback__form__right__content">
                             <div className="form__title">
                                 <p>Subscribe and get your bonus!</p>
                             </div>
-                            <form className='form__main'>
+                            <form className='form__main' onSubmit={handleForm}>
                                 <div className="form__input">
-                                    <input placeholder='Email address' type="email" name="" id="" />
+                                    <input placeholder='Email address' type="email" name="email" />
                                 </div>
                                 <div className="form__input">
-                                    <input placeholder='First name' type="text" name="" id="" />
+                                    <input placeholder='First name' type="text" name="firstName" />
                                 </div>
                                 <div className="form__input">
-                                    <input type="text" placeholder='Last name' name="" id="" />
+                                    <input type="text" placeholder='Last name' name="lastName" />
                                 </div>
                                 <div className="form__input">
-                                    <input type="text" placeholder='Address' name="" id="" />
+                                    <input type="text" placeholder='Address' name="address" />
                                 </div>
                                 
                                 <div className="form__input">
-                                    <button className='update__button'>Stay Updated</button>
+                                    <button type='submit' name='updateBtn' className='update__button'>Stay Updated</button>
                                 </div>
                             </form>
                         </div>
--- feedback-form-client/src/Component/Home/HomeLeft.js
+++ feedback-form-client/src/Component/Home/HomeLeft.js
@@ -4,20 +4,20 @@ import formImage from '../Assets/feedback__form__image__02.png';
 const HomeLeft = () => {
     return (
         <div className="feedback__form__left">
-        <div className="feedback__form__left__main">
-            <h1 className='heading'>
-                Get Everything you need<br></br>
-                for creating wordpress<br></br>
-                website
-            </h1>
-            <img className='form__image' src={formImage} alt="" />
-            <p className='feedback__form__bottom__text'>
-                All-in-one Multifunctional subscribe <br></br>
-                service perfect for launching all kinds of <br></br>
-                wordpress projects
-            </p>
+            <div className="feedback__form__left__main">
+                <h1 className='heading'>
+                    Get Everything you need<br></br>
+                    for creating wordpress<br></br>
+                    website
+                </h1>
+                <img className='form__image' src={formImage} alt="" />
+                <p className='feedback__form__bottom__text'>
+                    All-in-one Multifunctional subscribe <br></br>
+                    service perfect for launching all kinds of <br></br>
+                    wordpress projects
+                </p>
+            </div>
         </div>
-    </div>
     );
 };
 
