f358e046bc8d34cbf596530593f2653d567802ff
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-09-13 17:11:54 +0600

    Added Form All Data Table

--- feedback-form-client/src/Component/FormAllData/FormAllData.js
+++ feedback-form-client/src/Component/FormAllData/FormAllData.js
@@ -1,17 +1,37 @@
 import React, { useEffect, useState } from 'react';
+import './FormAllData.css';
 
 const FormAllData = () => {
     const [formData, setFormData] = useState([]);
-    useEffect(()=>{
+    useEffect(() => {
         fetch('https://feedback-form-server.vercel.app/form-all-data')
-        .then(res => res.json())
-        .then(data => setFormData(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setFormData(data))
+    }, [])
 
     console.log(formData)
     return (
-        <div>
-            
+        <div className='container form__all__data'>
+            <h1 className='form__all__data__heading'>Form All Data</h1>
+            <table className='table__main'>
+                <tr>
+                    <th>Emal</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Address</th>
+                    <th>Date</th>
+                </tr>
+                {
+                    formData.map(allData => 
+                    <tr>
+                        <td>{allData.email}</td>
+                        <td>{allData.firstName}</td>
+                        <td>{allData.lastName}</td>
+                        <td>{allData.address}</td>
+                        <td>{allData.date}</td>
+                    </tr>)
+                }
+            </table>
         </div>
     );
 };
